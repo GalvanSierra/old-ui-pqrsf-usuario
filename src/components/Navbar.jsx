a00a8a2c8f1ec411e1e1cae387ddf63c98e9d2e2
Practicante Sistemas <psistemas@hisantaana.com>
2023-11-08 08:59:09 -0500

    feat: :lipstick: change logo and icon html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "./../assets/react.svg";
+import logo from "./../assets/logo.jpg";
 import { useAuth } from "../hooks/useAuth";
 
 function Navbar() {
@@ -9,7 +9,12 @@ function Navbar() {
     { to: "/dashboard-pqrsf", text: "Dashboard", private: true },
     { to: "/login", text: "Login", private: false, publicOnly: true },
     { to: "/Logout", text: "Logout", private: true },
-    { to: "/dashboard-pqrsf-admin", text: "admin" },
+    {
+      to: "/dashboard-pqrsf-admin",
+      text: "admin",
+      private: true,
+      isAdmin: true,
+    },
   ];
 
   return (
@@ -21,6 +26,7 @@ function Navbar() {
             if (route.publicOnly && auth.user) return null;
 
             if (route.private && !auth.user) return null;
+
             return (
               <NavLink
                 key={route.to}
