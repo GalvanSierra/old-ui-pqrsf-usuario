79ad5110b77b2a19376bcb97ce837f07a5a42466
Practicante Sistemas <psistemas@hisantaana.com>
2023-12-04 07:43:44 -0500

    feat: change API URL

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: "http://localhost:3065/api/v1",
   headers: {
     Authorization: `Bearer ${token}`,
   },
