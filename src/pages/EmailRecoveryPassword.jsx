1b40efe7afa53018ca72c78d6ae0b577c867ef21
Practicante Sistemas <psistemas@hisantaana.com>
2023-12-11 14:04:12 -0500

    message in email recovery
    undefined

--- src/pages/EmailRecoveryPassword.jsx
+++ src/pages/EmailRecoveryPassword.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import api from "../service/api";
 import { Link } from "react-router-dom";
+import { SetMealSharp } from "@mui/icons-material";
 
 function EmailRecoveryPassword() {
   const {
@@ -10,14 +11,25 @@ function EmailRecoveryPassword() {
     formState: { errors },
   } = useForm();
 
-  const [isOkay, setIsOkay] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
   const onSubmit = async (data) => {
+    setLoading(true);
     await api
       .post("/auth/recovery", data)
       .then((response) => {
-        if (response.status === 200) setIsOkay(true);
+        if (response.status === 200) {
+          setLoading(false);
+          setIsOpen(true);
+          setMessage("Se envió un email para restablecer la contraseña");
+        }
       })
       .catch((error) => {
+        setLoading(false);
+        setIsOpen(true);
+        setMessage("error al enviar el email");
+
         console.error("Error en la solicitud POST", error);
       });
   };
@@ -55,7 +67,8 @@ function EmailRecoveryPassword() {
         <Link to="/login" className="login__link">
           Regresar al inicio de sesión
         </Link>
-        {isOkay && <p>El email se envió exitosamente a tu correo registrado</p>}
+        {loading && <p>Enviando...🔁</p>}
+        {isOpen && <p>{message}</p>}
       </div>
     </div>
   );
